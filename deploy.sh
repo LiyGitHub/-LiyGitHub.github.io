3848acebb80d0ec60db47d5c8bab4c5e3416d6b0
liy <761686949@qq.com>
2023-05-02 23:48:15 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://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@github.com/LiyGitHub/LiyGitHub.github.io.git master:gh-pages
+#git push -f https://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@github.com/LiyGitHub/LiyGitHub.github.io.git master:gh-pages
+git push -f https://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@github.com/LiyGitHub/LiyGitHub.github.io.git
 
 cd -
